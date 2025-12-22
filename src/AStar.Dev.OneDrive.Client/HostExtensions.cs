527b84618f5c1eafc6019fd53631f1df90119049
Jason <jason.barden1@outlook.com>
2025-12-22 07:30:46 +0000

    Update the UI a little more

--- src/AStar.Dev.OneDrive.Client/HostExtensions.cs
+++ src/AStar.Dev.OneDrive.Client/HostExtensions.cs
@@ -11,24 +11,24 @@ internal static class HostExtensions
     internal static void ConfigureApplicationServices(HostBuilderContext ctx, IServiceCollection services)
     {
         var dbPath = "Data Source=/home/jason/.config/astar-dev/astar-dev-onedrive-client/database/app.db"; // FIX THIS
-                var localRoot = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "OneDriveSync"); // AND THIS
-                var msalClientId = "3057f494-687d-4abb-a653-4b8066230b6e"; // CONFIG
-                _ = services.AddInfrastructure(dbPath, localRoot, msalClientId);
+        var localRoot = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "OneDriveSync"); // AND THIS
+        var msalClientId = "3057f494-687d-4abb-a653-4b8066230b6e"; // CONFIG
+        _ = services.AddInfrastructure(dbPath, localRoot, msalClientId);
 
-                // App services
-                _ = services.AddSyncServices(ctx.Configuration);
+        // App services
+        _ = services.AddSyncServices(ctx.Configuration);
 
-                // UI services and viewmodels
-                _ = services.AddSingleton<MainWindow>();
-                _ = services.AddSingleton<MainWindowViewModel>();
-                _ = services.AddSingleton<SettingsViewModel>();
-                _ = services.AddSingleton<DashboardViewModel>();
+        // UI services and viewmodels
+        _ = services.AddSingleton<MainWindow>();
+        _ = services.AddSingleton<MainWindowViewModel>();
+        _ = services.AddSingleton<SettingsViewModel>();
+        _ = services.AddSingleton<DashboardViewModel>();
 
-                // Sync settings
-                //_ = services.AddSingleton(new SyncSettings(ParallelDownloads: 4, BatchSize: 50));
-                ServiceProvider servicesProvider = services.BuildServiceProvider();
-                // LocalLogger = servicesProvider.GetRequiredService<Microsoft.Extensions.Logging.ILogger<Program>>();
-                Action<IServiceProvider> initializer = servicesProvider.GetRequiredService<Action<IServiceProvider>>();
-                initializer(servicesProvider);
+        // Sync settings
+        //_ = services.AddSingleton(new SyncSettings(ParallelDownloads: 4, BatchSize: 50));
+        ServiceProvider servicesProvider = services.BuildServiceProvider();
+        // LocalLogger = servicesProvider.GetRequiredService<Microsoft.Extensions.Logging.ILogger<Program>>();
+        Action<IServiceProvider> initializer = servicesProvider.GetRequiredService<Action<IServiceProvider>>();
+        initializer(servicesProvider);
     }
 }
